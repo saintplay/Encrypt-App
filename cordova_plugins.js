a97903ef8fdbef4de29d68f19c028e3e4f27a2f0
Diego Jara <saintplay96@gmail.com>
2016-07-16 15:45:57 -0500

    Release - version 1.0.2 para browser

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -16,13 +16,29 @@ module.exports = [
         "merges": [
             ""
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/src/browser/SplashScreenProxy.js",
+        "id": "cordova-plugin-splashscreen.SplashScreenProxy",
+        "pluginId": "cordova-plugin-splashscreen",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-inappbrowser": "1.4.0",
-    "cordova-plugin-whitelist": "1.2.2"
+    "cordova-plugin-splashscreen": "3.2.2",
+    "cordova-custom-config": "2.0.2"
 }
 // BOTTOM OF METADATA
 });
